55d820eaa88744db4a0717cc26ca16f2154209bd
Lauren Huynh <laurenhuynh@Laurens-MacBook-Pro.local>
2022-11-14 17:33:08 +1100

    global styles update

--- styles/Global.js
+++ styles/Global.js
@@ -12,17 +12,6 @@ export const containers = StyleSheet.create({
     width: "100%",
     height: 135,
   },
-export const containers = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: 'center',
-    paddingHorizontal: 16
-  },
-  header: {
-    width: "100%",
-    height: 135,
-  },
 });
 
 export const fonts = StyleSheet.create({
